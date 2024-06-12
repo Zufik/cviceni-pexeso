e5ee739126ac3af0777ddd40160833cd526558ff
Lenka Zufala <m-lenkazufala@seznam.cz>
2024-06-12 14:23:32 +0200

    a

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 console.log('Funguju!')
 
-const karticky = document.querySelectorAll('.karticka')
+const karticky = document.querySelectorAll('.karticka').forEach ((element)=>{
+    element.addEventListener ("click",(evt)=> {
+        evt.target.classList.add("otocena")
+    })
+})
 
-karticky[0].classList.remove('otocena')
+//karticky[0].classList.remove('otocena')
+
